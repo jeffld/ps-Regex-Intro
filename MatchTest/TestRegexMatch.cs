35efade0950526871e2daea33bf20ea3b75c0bca
Jeff Darling <jeffld3@gmail.com>
2017-01-31 10:27:45 -0600

    added more test functions

--- MatchTest/TestRegexMatch.cs
+++ MatchTest/TestRegexMatch.cs
@@ -44,8 +44,134 @@ namespace RegexIntro.Tests
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod()]
+        public void SimpleMatchConcatenate2()
+        {
+            // Arrange
+            var RegexIntro = new RegexIntro();
+            RegexIntro.Pattern = "cat";
+            RegexIntro.Subject = "catcat";
+            var expected = "True  @0:3";
+
+            // Act
+            var actual = RegexIntro.MatchTest();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SimpleMatchAlternation1()
+        {
+            // Arrange
+            var RegexIntro = new RegexIntro();
+            RegexIntro.Pattern = "cat|dog";
+            RegexIntro.Subject = "cat";
+            var expected = "True  @0:3";
+
+            // Act
+            var actual = RegexIntro.MatchTest();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SimpleMatchAlternation2()
+        {
+            // Arrange
+            var RegexIntro = new RegexIntro();
+            RegexIntro.Pattern = "cat|dog";
+            RegexIntro.Subject = "dog";
+            var expected = "True  @0:3";
+
+            // Act
+            var actual = RegexIntro.MatchTest();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SimpleMatchAlternation3()
+        {
+            // Arrange
+            var RegexIntro = new RegexIntro();
+            RegexIntro.Pattern = "cat|catnip";
+            RegexIntro.Subject = "cat";
+            var expected = "True  @0:3";
+
+            // Act
+            var actual = RegexIntro.MatchTest();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SimpleMatchAlternation4()
+        {
+            // Arrange
+            var RegexIntro = new RegexIntro();
+            RegexIntro.Pattern = "cat|catnip";
+            RegexIntro.Subject = "catnip";
+            var expected = "True  @0:3";
+
+            // Act
+            var actual = RegexIntro.MatchTest();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SimpleMatchAlternation5()
+        {
+            // Arrange
+            var RegexIntro = new RegexIntro();
+            RegexIntro.Pattern = "catnip|cat";
+            RegexIntro.Subject = "catnip";
+            var expected = "True  @0:6";
+
+            // Act
+            var actual = RegexIntro.MatchTest();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
 
 
+        [TestMethod()]
+        public void SimpleMatchRepetition1()
+        {
+            // Arrange
+            var RegexIntro = new RegexIntro();
+            RegexIntro.Pattern = "a*";
+            RegexIntro.Subject = "za";
+            var expected = "True  @0:0";
+
+            // Act
+            var actual = RegexIntro.MatchTest();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SimpleMatchRepetition2()
+        {
+            // Arrange
+            var RegexIntro = new RegexIntro();
+            RegexIntro.Pattern = "(cat)*";
+            RegexIntro.Subject = "catcat";
+            var expected = "True  @0:6";
+
+            // Act
+            var actual = RegexIntro.MatchTest();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
 
     }
 
