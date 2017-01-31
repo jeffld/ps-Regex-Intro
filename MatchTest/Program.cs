2e133226732a8171133d892eb81f2a35b7461b15
Jeff Darling <jeffld3@gmail.com>
2017-01-31 06:52:21 -0600

    Initial Commit
    Add exercise files

---
+++ MatchTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MatchTest
+{
+    class Program
+    {
+        private const string MatchSuccess = "{0}  @{1}:{2}";
+        static void Main(string[] args)
+        {
+            var pattern = args[0];
+            var subject = args[1];
+            var regex = new Regex(pattern);
+            var match = regex.Match(subject);
+            if(match.Success)
+            {
+                Console.WriteLine(MatchSuccess, match.Success, match.Index, match.Length);                
+            }
+            else
+            {
+                Console.WriteLine(match.Success);                
+            }
+        }
+    }
+}
